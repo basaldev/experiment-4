30342102f2120168ed43ec224c91a377bb4b1c38
Shinsuke Marty Yamada <marty@exodevhub.com>
2019-03-15 03:56:48 +0900

    develop

--- static/main.js
+++ static/main.js
@@ -14,33 +14,46 @@ document.addEventListener("DOMContentLoaded", function(event) {
     'Put the scalpel to the affected area with right hand.',
     'You passed this test!'
   ];
-
+  var $left = document.getElementById('left');
+  var $right = document.getElementById('right');
+  var $message = document.getElementById('message');
+  var $reload = document.getElementById('reload');
+  var leftInitPosition = $left.getAttribute('position').split(' ');
+  var leftInitotation = $left.getAttribute('rotation').split(' ');
+  var rightInitPosition = $right.getAttribute('position').split(' ');
+  var rightInitotation = $right.getAttribute('rotation').split(' ');
+  var initPosition = [
+    { x: leftInitPosition[0], y: leftInitPosition[1], z: leftInitPosition[2] },
+    { x: rightInitPosition[0], y: rightInitPosition[1], z: rightInitPosition[2] },
+  ];
+  var initRotation = [
+    { x: leftInitotation[0], y: leftInitotation[1], z: leftInitotation[2] },
+    { x: rightInitotation[0], y: rightInitotation[1], z: rightInitotation[2] },
+  ];
   function transition() {
     if (step < messages.length - 1) {
       step++;
     } else {
       step = 0;
     }
-    var $message = document.getElementById('message');
     $message.setAttribute('value', messages[step]);
   }
 
   function reset() {
     // socket.emit('reload');
-    prev = {};
+    prevPosition = {};
+    prevRotation = {};
     step = 0;
-    $left = document.getElementById('left');
-    $right = document.getElementById('right');
-    $message = document.getElementById('message');
-    $left.setAttribute('rotation', {x: 7.56, y: -10.53, z: 90 });
-    $left.setAttribute('position', {x: -1.05413, y: 0.04497, z: -2.42308});
-    $right.setAttribute('rotation', {x: 7.56, y: -8.21, z: 90 });
-    $right.setAttribute('position', {x: 0.86416, y: -0.00792, z: -2.41991});
+    console.log(initPosition, initRotation)
+    $left.setAttribute('position', initPosition[0]);
+    $left.setAttribute('rotation', initRotation[0]);
+    $right.setAttribute('position', initPosition[1]);
+    $right.setAttribute('rotation', initRotation[1]);
     $message.setAttribute('value', 'Hold the affected area with left hand')
   }
 
   function main(channel, remote1, remote2) {
-    $hand = document.getElementById(channel);
+    var $hand = document.getElementById(channel);
     var position = $hand.getAttribute('position');
     var rotation = $hand.getAttribute('rotation');
     if (typeof prevPosition[channel] === 'undefined' || typeof prevRotation[channel] === 'undefined') {
@@ -82,9 +95,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
       main(channel, remote1, remote2);
     });
 
-    var $reload = document.getElementById('reload');
     $reload.addEventListener('click', function() {
       reset();
+    });
+    $reload.addEventListener('mouseenter', function() {
+      $reload.classList.add('active');
+    });
+    $reload.addEventListener('mouseleave', function() {
+      $reload.classList.remove('active');
     });
   });
 });
