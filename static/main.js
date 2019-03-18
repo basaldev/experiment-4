0372cff6410d8f5d3d4c30377497e3ddbf5c99be
Shinsuke Marty Yamada <marty@exodevhub.com>
2019-03-18 15:20:02 +0900

    Improve setup page

--- static/main.js
+++ static/main.js
@@ -1,17 +1,17 @@
 var $vrUrl = document.getElementById('vrUrl');
 var vrUrl = location.href + $vrUrl.innerText;
 $vrUrl.innerText = vrUrl;
-new QRCode(document.getElementById('vrQr'), vrUrl);
+new QRCode(document.getElementById('vrQr'), { text: vrUrl, width: 200, height: 200, correctLevel : QRCode.CorrectLevel.H });
 
 var $leftUrl = document.getElementById('leftUrl');
 var leftUrl = location.href + $leftUrl.innerText;
 $leftUrl.innerText = leftUrl;
-new QRCode(document.getElementById('leftQr'), leftUrl);
+new QRCode(document.getElementById('leftQr'), { text: leftUrl, width: 200, height: 200, correctLevel : QRCode.CorrectLevel.H });
 
 var $rightUrl = document.getElementById('rightUrl');
 var rightUrl = location.href + $rightUrl.innerText;
 $rightUrl.innerText = rightUrl;
-new QRCode(document.getElementById('rightQr'), rightUrl);
+new QRCode(document.getElementById('rightQr'), { text: rightUrl, width: 200, height: 200, correctLevel : QRCode.CorrectLevel.H });
 
 var socket = io();
 var state = {
@@ -23,7 +23,12 @@ var state = {
 socket.on('connect', function() {
   socket.on('device-ready', function(channel) {
     var $state = document.getElementById(channel + 'State');
-    $state.innerText = 'Ready';
+    $state.innerHTML =
+      'Ready <br>' +
+      '<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">' +
+      '<circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" />' +
+      '<path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />' +
+      '</svg>';
     $state.classList.remove('error');
     $state.classList.add('success');
     state[channel] = true;
